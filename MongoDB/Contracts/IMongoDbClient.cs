78eecfc7cf4f9dd6a6e0831563ec7e82e2b716d3
Nitin Sapru <sapru-nitin@outlook.com>
2020-02-24 20:50:13 +0530

    Added new methods to the client to get database, collection & list of items in a collection.

--- MongoDB/Contracts/IMongoDbClient.cs
+++ MongoDB/Contracts/IMongoDbClient.cs
@@ -25,5 +25,32 @@ namespace MongoDB.Contracts
         /// <param name="databaseName">The name of the database in which the collections are present.</param>
         /// <returns>The list of collections names in MongoDB database.</returns>
         Task<IEnumerable<string>> ListCollectionsAsync(string databaseName);
+
+        /// <summary>
+        ///     Gets the collection using collection name & database name.
+        /// </summary>
+        /// <typeparam name="T">The type of object which replicates the JSON body of the document.</typeparam>
+        /// <param name="database">The database name.</param>
+        /// <param name="collection">The collection name.</param>
+        /// <returns>The collection object.s.</returns>
+        IMongoCollection<T> GetCollection<T>(string database, string collection);
+
+        /// <summary>
+        ///     Gets the list of documents from a collection.
+        /// </summary>
+        /// <typeparam name="T">The type which replicates the JSON model of the document.</typeparam>
+        /// <param name="database">The database name.</param>
+        /// <param name="collection">The collection name.</param>
+        /// <returns>The list of items present in collection.</returns>
+        Task<IEnumerable<T>> ListDocumentsFromCollection<T>(string database, string collection);
+
+        /// <summary>
+        ///     Gets the list of documents from a collection.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="mongoCollection"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> ListDocumentsFromCollection<T>(IMongoCollection<T> mongoCollection, FilterDefinition<T> filter);
     }
 }
--- MongoDB/MongoDbClient.cs
+++ MongoDB/MongoDbClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 using MongoDB.Contracts;
 using MongoDB.Driver;
 using MongoDB.Exceptions;
@@ -14,7 +15,6 @@ namespace MongoDB
         public MongoDbClient(string connectionString)
         {
             connectionString.ShouldNotBeNullOrEmpty();
-
             this.mongoDB = new MongoClient(connectionString);
         }
 
@@ -61,6 +61,75 @@ namespace MongoDB
                     .ListCollectionNamesAsync().ConfigureAwait(false);
 
                 return await collections.ToListAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                throw new MongoDbClientException(ex);
+            }
+        }
+
+        /// <summary>
+        ///     Gets the collection using collection name & database name.
+        /// </summary>
+        /// <typeparam name="T">The type of object which replicates the JSON body of the document.</typeparam>
+        /// <param name="database">The database name.</param>
+        /// <param name="collection">The collection name.</param>
+        /// <returns>The collection object.s.</returns>
+        public IMongoCollection<T> GetCollection<T>(string database, string collection)
+        {
+            database.ShouldNotBeNullOrEmpty();
+            collection.ShouldNotBeNullOrEmpty();
+
+            try
+            {
+                return this.mongoDB.GetDatabase(database).GetCollection<T>(collection);
+            }
+            catch (Exception ex)
+            {
+                throw new MongoDbClientException(ex);
+            }
+        }
+
+        /// <summary>
+        ///     Gets the list of documents from a collection.
+        /// </summary>
+        /// <typeparam name="T">The type which replicates the JSON model of the document.</typeparam>
+        /// <param name="database">The database name.</param>
+        /// <param name="collection">The collection name.</param>
+        /// <returns>The list of items present in collection.</returns>
+        public async Task<IEnumerable<T>> ListDocumentsFromCollection<T>(string database, string collection)
+        {
+            database.ShouldNotBeNullOrEmpty();
+            collection.ShouldNotBeNullOrEmpty();
+
+            try
+            {
+                var collectionObject = this.mongoDB.GetDatabase(database).GetCollection<T>(collection);
+                var items = collectionObject.Find(collection);
+
+                return await items.ToListAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                throw new MongoDbClientException(ex);
+            }
+        }
+
+        /// <summary>
+        ///     Gets the list of documents from a collection.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="mongoCollection"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<T>> ListDocumentsFromCollection<T>(IMongoCollection<T> mongoCollection, FilterDefinition<T> filter)
+        {
+            mongoCollection.ShouldNotBeNull();
+            filter.ShouldNotBeNull();
+
+            try
+            {
+                return await mongoCollection.Find(filter).ToListAsync().ConfigureAwait(false);
             }
             catch (Exception ex)
             {
